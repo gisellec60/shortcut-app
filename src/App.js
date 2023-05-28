e16d2c9f4aba21483440909131439c9cdb032dd5
Giselle <gisellec60@gmail.com>
2023-05-28 18:38:31 -0400

    initial page layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
 
+function BasicEditing () {
+  return (
+    <div className = "box" id="basic">
+     <p>Basic Editing</p>
+    </div>
+  )
+}
+function Add () {
+  return (
+    <div className = "edit" id="add">
+     <p>Add</p>
+    </div>
+  )
+}
+function Modify () {
+  return (
+    <div className = "edit" id="mod">
+     <p>Modify</p>
+    </div>
+  )
+}
+function Delete () {
+  return (
+    <div className = "edit" id="del">
+     <p>Delete</p>
+    </div>
+  )
+}
+function MultiCursor () {
+  return (
+    <div className = "box" id="multi">
+     <p>Multi-Cursor and Selection</p>
+    </div>
+  )
+}
+function General () {
+  return (
+    <div className = "box" id="gen">
+     <p>General</p>
+    </div>
+  )
+}
+
+function SearchReplace () {
+  return (
+    <div className = "box" id="search">
+      <p>Search and Replace</p>
+    </div>
+  )
+}
+function SearchBar () {
+  return (
+    <form  style = {{padding:"5px", border:"5px solid blue", display:"flex",flexdirection:"column"}}>
+    <input type="text" placeholder="Enter search phrase..." />
+    </form>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 >
+        <p> VS Code Shortcuts for Windows </p>
+      </h1>
+       <SearchBar />
+       <General />
+       <BasicEditing />
+       <SearchReplace />
+       <MultiCursor />
+       <Add />
+       <Modify />
+       <Delete />
+
     </div>
   );
 }
+
 
 export default App;
