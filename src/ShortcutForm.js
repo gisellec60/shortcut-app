d0838efb71afdf19a4bfc53a0821b2afb1926d8a
Giselle <gisellec60@gmail.com>
2023-06-18 17:07:56 -0400

    renamed data

--- src/ShortcutForm.js
+++ src/ShortcutForm.js
@@ -2,7 +2,7 @@ import React,{useState} from "react"
 
 function ShortcutForm ({isAdd}) {
     const [selected, setSelected] = useState("")
-    const [formData, setFormData] = useState({command:"",keys:""})
+    const [formData, setFormData] = useState({task:"",keys:""})
 
     const handleSubmit = () => {
 
@@ -12,11 +12,11 @@ function ShortcutForm ({isAdd}) {
       <section id="container">
         <h1>{isAdd ? "Add Shortcut" : "Modify Shortcut"}</h1>
         <form onSubmit={handleSubmit}>
-            <label>Command
-              <input type="text" name="command" value={formData.command}/>
+            <label className="label">Task
+              <input className="inputsize" type="text" name="task" value={formData.task}/>
             </label>
-            <label> Keys
-            <input type="text" name="keys" value={formData.keys} />
+            <label className="label"> Keys
+            <input className="inputsize" type="text" name="keys" value={formData.keys} />
             </label>    
             <h3 id="pick-category">Pick a Category </h3>
             <select className="menu-trigger" value={selected} onChange={(e) =>setSelected(e.target.value)} > 
