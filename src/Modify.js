fa6e0c2107308d4d5f6d6fba8c0a51fab976cd69
Giselle <gisellec60@gmail.com>
2023-06-20 15:51:11 -0400

    made changes to handleChange

--- src/Modify.js
+++ src/Modify.js
@@ -11,25 +11,24 @@ function Modify() {
     category.current = e.target.value
     if(category.current === "Search and Replace")
        category.current = "search"
-     
     fetch(`http://localhost:3001/${category.current.toLowerCase()}`)
-      .then((res) => res.json())
-      .then((shortcuts) => setShortcuts(shortcuts))
-    }
-    
-    const showModal =((id)=> {
-       console.log(id)
-       return <Modal closeModal={setOpenModal} id={id} />
-    })  
+    .then((res) => res.json())
+    .then((shortcuts) => setShortcuts(shortcuts))
+  }
+   
+  const showModal =(id) => {
+    console.log(id)
+    return <Modal closeModal={setOpenModal} id={id} />
+  }  
 
-    const displayInfo = shortcuts.map((shortcut) => {
-      return (
-       <div >
-          <div className="listing" >
-            <div key={shortcut.id}>{shortcut.task}: {shortcut.keys}</div> 
-             <button onClick={() => { 
-               showModal(shortcut.id); setOpenModal(true);}} className ='editButn'>Edit</button> 
-           </div>
+  const displayInfo = shortcuts.map((shortcut) => {
+    return (
+      <div >
+        <div className="listing" >
+          <div key={shortcut.id}>{shortcut.task}: {shortcut.keys}</div> 
+           <button onClick={() => { 
+             showModal(shortcut.id); setOpenModal(true);}} className ='editButn'>Edit</button> 
+         </div>
       </div>   
     )
   })
@@ -45,22 +44,10 @@ function Modify() {
             <option>Search and Replace</option>
             <option>Multi-Cursor</option>
           </select>
-            {displayInfo}
+           {displayInfo}
           {/* {openModal && <Modal closeModal={setOpenModal}  />} */}
    </div>
   )
 }
 
 export default Modify                                  
-// {/* <div key={shortcut.id} id="editListing">
-//         <label className="label">Task
-//            <input className="inputsize" type="text" name="task" 
-//                 onChange={handleChangeKeys}  value={shortcut.task}/>
-//         </label>
-//         {/* <button onClick={() => handleEditClick(shortcut.id)}> Edit </button> */}
-//         <label className="label"> Keys
-//            <input className="inputsize" type="text" name="keys"
-//                onChange={handleChangeKeys} value={shortcut.keys} />
-//         </label> 
-//         {/* <button onClick={() => handleEditClick(shortcut.id)}> Edit </button> */}
-//      </div> */}
