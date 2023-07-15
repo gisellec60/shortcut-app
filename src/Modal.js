b563a1208bdf5072c2c32638bde21b5428750394
Giselle <gisellec60@gmail.com>
2023-07-15 16:16:04 -0400

    removed console.log

--- src/Modal.js
+++ src/Modal.js
@@ -12,8 +12,7 @@ function Modal({closeModal, patchId, patchShortcut, patchKey, patchUpdate,
       
         const handleSubmit = ((e) => {
            e.preventDefault()
-           console.log(formData)  
-        fetch(`https://gisellec60-json-server-template.onrender.com/shortcuts/${patchId}`, {
+           fetch(`https://gisellec60-json-server-template.onrender.com/shortcuts/${patchId}`, {
         // fetch(`https://localhost:3000/shortcuts/${patchId}`, {
             method:"PATCH",
             headers: {"Content-Type" : "application/json"},
@@ -23,8 +22,7 @@ function Modal({closeModal, patchId, patchShortcut, patchKey, patchUpdate,
          })
          .then(res => res.json())
          .then(obj => patchUpdate(obj))
-         .then(obj => console.log("what is this", obj))
-        //  alert("update succesful")
+         alert("update succesful")
          closeModal(false)
     })
 
